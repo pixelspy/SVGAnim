f2d4d5312a966604b2e4a020f0f9537ff71e6c57
mahana <mahana.delacour@gmail.com>
2017-12-29 14:43:50 +0100

    card html

--- script.js
+++ script.js
@@ -177,51 +177,51 @@ function modalContent2() {
 			img: "./img/informaticien-1-turing.png",
 			id: 1
 		},
-		// {
-		// 	name: "lee",
-		// 	img: "./img/informaticien-2-berners-lee.png",
-		// 	id: 2
-		// },
-		// {
-		// 	name: "hopper",
-		// 	img: "./img/informaticien-3-hopper.png",
-		// 	id: 3
-		// },
-		// {
-		// 	name: "lovelace",
-		// 	img: "./img/informaticien-4-lovelace.png",
-		// 	id: 4
-		// },
-		// {
-		// 	name: "babbage",
-		// 	img: "./img/informaticien-5-Babbage.png",
-		// 	id: 5
-		// },
-		// {
-		// 	name: "stroustrup",
-		// 	img: "./img/informaticien-6-stroustrup.png",
-		// 	id: 6
-		// },
-		// {
-		// 	name: "ritchie",
-		// 	img: "./img/informaticien-7-ritchie.png",
-		// 	id: 7
-		// },
-		// {
-		// 	name: "?",
-		// 	img: "./img/informaticien-8-??.png",
-		// 	id: 8
-		// },
-		// {
-		// 	name: "torsvalds",
-		// 	img: "./img/informaticien-9-torsvlads.jpg",
-		// 	id: 9
-		// },
-		// {
-		// 	name: "solo",
-		// 	img: "./img/informaticien-10-solo.jpg",
-		// 	id: 10
-		// },
+		{
+			name: "lee",
+			img: "./img/informaticien-2-berners-lee.png",
+			id: 2
+		},
+		{
+			name: "hopper",
+			img: "./img/informaticien-3-hopper.png",
+			id: 3
+		},
+		{
+			name: "lovelace",
+			img: "./img/informaticien-4-lovelace.png",
+			id: 4
+		},
+		{
+			name: "babbage",
+			img: "./img/informaticien-5-Babbage.png",
+			id: 5
+		},
+		{
+			name: "stroustrup",
+			img: "./img/informaticien-6-stroustrup.png",
+			id: 6
+		},
+		{
+			name: "ritchie",
+			img: "./img/informaticien-7-ritchie.png",
+			id: 7
+		},
+		{
+			name: "?",
+			img: "./img/informaticien-8-??.png",
+			id: 8
+		},
+		{
+			name: "torsvalds",
+			img: "./img/informaticien-9-torsvlads.jpg",
+			id: 9
+		},
+		{
+			name: "solo",
+			img: "./img/informaticien-10-solo.jpg",
+			id: 10
+		},
 	];
 
 	Memory.init(cards);
